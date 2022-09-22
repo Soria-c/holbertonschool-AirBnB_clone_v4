c0145c2e7bb4244db19e51548d38197cd43cf292
Carlos Soria <soriac367@gmail.com>
2022-09-22 14:13:30 -0500

    Implemented checkbox event

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,13 @@
-$('document').ready(function () {
-  
-})
+window.onload = function() {
+    let amenities = {};
+
+    $("input#input-amenity").on('change', function() {
+      if( $(this).is(':checked') ) {
+          amenities[$(this)[0].attributes["data-id"].nodeValue] = $(this)[0].attributes["data-name"].nodeValue;
+      } else {
+          delete amenities[$(this)[0].attributes["data-id"].nodeValue];
+      }
+      if (Object.keys(amenities).length) $(".amenities h4").text(Object.values(amenities).join(", "));
+    });
+
+};
