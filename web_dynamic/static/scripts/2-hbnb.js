e7be285bd0a43c2ded8079f072969c57ff36af2b
Carlos Soria <soriac367@gmail.com>
2022-09-23 11:43:37 -0500

    Debug

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,24 +1,31 @@
-window.onload = function() {
+window.onload = function () {
   let amenities = {};
 
-  $("input#input-amenity").on('change', function() {
-    if( $(this).is(':checked') ) {
-        amenities[$(this)[0].attributes["data-id"].nodeValue] = $(this)[0].attributes["data-name"].nodeValue;
+  $("input#input-amenity").on("change", function () {
+    if ($(this).is(":checked")) {
+      amenities[$(this)[0].attributes["data-id"].nodeValue] =
+        $(this)[0].attributes["data-name"].nodeValue;
     } else {
-        delete amenities[$(this)[0].attributes["data-id"].nodeValue];
+      delete amenities[$(this)[0].attributes["data-id"].nodeValue];
     }
-    if (Object.keys(amenities).length) $(".amenities h4").text(Object.values(amenities).join(", "));
+    if (Object.keys(amenities).length)
+      $(".amenities h4").text(Object.values(amenities).join(", "));
   });
 
-};
-$.ajax({
-  type: "GET",
-  url: "http://0.0.0.0:5001/api/v1/status/",
-  success: function getStatus (data) {
-    if (data.status === "OK") {
-      $("div#api_status").addClass("available");
-    } else {
-      $("div#api_status").removeClass("available");
+  $.ajax({
+    type: "GET",
+    url: "http://0.0.0.0:5001/api/v1/status/",
+    success: function (data) {
+      
+      if (data.status === "OK") {
+        console.log(data.status);
+        $("#api_status").addClass("available");
+      } else {
+        $("#api_status").removeClass("available");
+      }
+    },
+    error: function() {
+      alert('Error API not available');
     }
-  }
-});
+  });
+};
